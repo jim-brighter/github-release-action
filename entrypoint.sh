c1c73825321d038eee5e765da0d77a0644d746ec
Jim Brighter <jbrighter92@gmail.com>
2024-05-29 13:49:10 -0400

    change tag search

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,23 +2,20 @@
 
 git config --system --add safe.directory /github/workspace
 
-last_release=$(git describe --abbrev=0 --tags)
-if [[ $? -ne 0 ]]; then
-  echo "No previous release found, defaulting to 0.0.0"
-  last_release="0.0.0"
-fi
+major_version=$1
+minor_version=$2
 
-new_major_version=$1
-new_minor_version=$2
+last_release=$(git tag -l "v$major_version.$minor_version*" --sort=-v:refname | head -n 1)
+last_release=${last_release:="v0.0.0"}
 
 if [[ ! -z "$3" ]]; then
-  new_patch_version=$3
+  patch_version=$3
 else
   last_patch_version=$(echo $last_release | cut -d '.' -f 3)
-  new_patch_version=$((last_patch_version+1))
+  patch_version=$((last_patch_version+1))
 fi
 
-new_release="v$new_major_version.$new_minor_version.$new_patch_version"
+new_release="v$major_version.$minor_version.$patch_version"
 
 echo "Last Release: $last_release"
 echo "New Release: $new_release"
